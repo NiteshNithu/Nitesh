e7d7b6a055949ffeb77bd7ea0728862b10f5e62c
NiteshNithu <105484874+NiteshNithu@users.noreply.github.com>
2022-05-13 13:58:43 +0530

    Update hellow.java
    changes

--- hello/src/hello/hellow.java
+++ hello/src/hello/hellow.java
@@ -4,7 +4,8 @@ public class hellow {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hellow");
+		string s="hellow"
+System.out.println(s);
 	}
 
 }
